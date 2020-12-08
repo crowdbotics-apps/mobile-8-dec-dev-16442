7ce85279193747d9ce0a40b9ff246f3068927dc5
Crowdbotics <team@crowdbotics.com>
2020-12-08 06:33:10 +0000

    Update models

--- backend/hgjhg/migrations/0001_initial.py
+++ backend/hgjhg/migrations/0001_initial.py
@@ -7,18 +7,25 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Userhgfhgf',
+            name="Userhgfhgf",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('username', models.CharField(max_length=30)),
-                ('password', models.CharField(max_length=30)),
-                ('email', models.EmailField(max_length=254)),
-                ('name', models.CharField(max_length=200)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("username", models.CharField(max_length=30)),
+                ("password", models.CharField(max_length=30)),
+                ("email", models.EmailField(max_length=254)),
+                ("name", models.CharField(max_length=200)),
             ],
         ),
     ]
--- backend/hgjhg/models.py
+++ backend/hgjhg/models.py
@@ -7,15 +7,6 @@ class Userhgfhgf(models.Model):
     username = models.CharField(
         max_length=30,
     )
-    password = models.CharField(
-        max_length=30,
-    )
-    email = models.EmailField(
-        max_length=254,
-    )
-    name = models.CharField(
-        max_length=200,
-    )
 
 
 # Create your models here.
